4237a26455fd7cc22cd313b9ae505cb724722ca0
dpociask <pociask1992@gmail.com>
2020-11-11 13:25:22 +0100

    MockMvc example - RecipeControllerTest refactoring

--- src/test/java/guru/springframework/controller/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controller/RecipeControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +16,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class RecipeControllerTest {
 
@@ -28,6 +33,15 @@ class RecipeControllerTest {
         MockitoAnnotations.initMocks(this);
 
         recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void testWebMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        mockMvc.perform(get("/recipe/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/recipe/index"));
     }
 
     @Test
